1745c13c32ddc9b70d9d9978dd5165a1c9976cee
dhirajNS <dhiraj.shetty@neosoftmail.com>
2023-11-28 19:41:36 +0530

    -contains partial impl

---
+++ src/app/search-delete/search-delete.component.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+@Component({
+  selector: 'app-search-delete',
+  standalone: true,
+  imports: [CommonModule],
+  templateUrl: './search-delete.component.html',
+  styleUrl: './search-delete.component.css'
+})
+export class SearchDeleteComponent {
+
+}
